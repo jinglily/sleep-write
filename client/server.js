609a9be97b8735acfc3b24318f6501e704c5ebc1
jingli <351201853@qq.com>
2016-12-20 10:23:27 +0800

    i

--- client/server.js
+++ client/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
-app.get('/',function(req,res){
+// 用跟本文件平级的一个 public 夹作为静态文件的存放位置
+// 没有这一行，后面 sendFile 的 index.html 就找不到了。
+
+app.get('*',function(req,res){
   console.log('success');
-  res.sendFile('index.html');
+  res.sendFile('index.html',{root:'public'});
 })
 
 app.listen(4000,function(){
